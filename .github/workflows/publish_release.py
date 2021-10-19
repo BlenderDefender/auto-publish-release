269b0778c66ed684acd5f7afe97cf8d9e920b2d2
Blender Defender <defenderblender@gmail.com>
2021-10-19 22:36:21 +0200

    #RELEASE

--- .github/workflows/publish_release.py
+++ .github/workflows/publish_release.py
@@ -1,5 +1,15 @@
+import argparse
+
 import os
 from os import path as p
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--commit_message")
+args = parser.parse_args()
+
+commit_message = args.commit_message
+version_raw = commit_message.split("#RELEASE")[1]
+version = version_raw.replace(".", "_")
 
 if not "README.md" in os.listdir():
   with open("README.md", "w+") as f:
@@ -9,7 +19,6 @@ if not "README.md" in os.listdir():
 with open("README.md", "r") as f:
   text = f.read()
 
-
 changelog_text = text.split("<!-- CHANGELOG -->")[1]
 
 prev_text = ""
@@ -17,7 +26,7 @@ if "CHANGELOG.md" in os.listdir():
   with open("CHANGELOG.md", "r") as f:
     prev_text = f.read()
 
-final_text = f"""## Version 
+final_text = f"""## Version {version_raw}
 {changelog_text}
 
 {prev_text}
