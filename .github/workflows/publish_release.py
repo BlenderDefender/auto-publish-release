fb3af6f2035d6d9f1d108320ea5f3593230c6702
Blender Defender <defenderblender@gmail.com>
2021-10-19 22:14:08 +0200

    #RELEASE

--- .github/workflows/publish_release.py
+++ .github/workflows/publish_release.py
@@ -1,7 +1,14 @@
 import os
 from os import path as p
 
-print(os.listdir())
+if not "README.md" in os.listdir():
+  with open("README.md", "w+") as f:
+    f.write("<!-- CHANGELOG -->\n\n<!-- CHANGELOG -->")
+  exit()
+  
+ with open ("README.md", "r") as f:
+  text = f.read()
 
 
-# changelog_text = text.split("<!-- CHANGELOG -->")
+changelog_text = text.split("<!-- CHANGELOG -->")[1]
+print(changelog_text)
